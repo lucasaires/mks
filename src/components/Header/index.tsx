f544a9c1da61402eb0b8a910bee5101cb6b094f0
Lucas <lucas.aires@ccc.ufcg.edu.br>
2023-01-28 12:55:50 -0300

    refactor header jsx

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -6,7 +6,7 @@ import ShopDetail from "../ShopDetail";
 import { useAppDispatch, useAppSelector } from "../../hooks/useTypedSelectort";
 import { useEffect, useState } from "react";
 
-const Header = (): JSX.Element | JSX.Element[] => {
+const Header = (): JSX.Element => {
   const { open, toggleOpen } = useShopListContext();
   const dispatch = useAppDispatch();
 
